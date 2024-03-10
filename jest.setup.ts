8e329bb315ef3a5e0a35f5750205dff1109c938f
Alexey Bachmanov <alexbachmanov@gmail.com>
2024-03-09 20:16:11 -0500

    Removed msw as a dependency and removed code that relies of it

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,7 +1,6 @@
 // imports jest-dom for ALL tests
 import '@testing-library/jest-dom';
 import dotenv from 'dotenv';
-import { server } from './mocks/node';
 
 // set up environment variables
 dotenv.config({ path: './.env.local' });
@@ -10,21 +9,3 @@ dotenv.config({ path: './.env.local' });
 
 // suppress jest vs. mongoose warnings
 process.env.SUPPRESS_JEST_WARNINGS = 'true';
-
-// setup mock service worker
-beforeAll(() => {
-  // Enable API mocking before all the tests.
-  server.listen();
-});
-
-afterEach(() => {
-  // Reset the request handlers between each test.
-  // This way the handlers we add on a per-test basis
-  // do not leak to other, irrelevant tests.
-  server.resetHandlers();
-});
-
-afterAll(() => {
-  // Finally, disable API mocking after the tests are done.
-  server.close();
-});
