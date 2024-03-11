c7c675628116574b25abf62c093b7d7ac50bec28
Alexey Bachmanov <alexbachmanov@gmail.com>
2024-03-10 22:43:41 -0400

    Removed jest.polyfills.ts - it was causing compile errors. Reconfigured jest to match.

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,5 @@
-// imports jest-dom for ALL tests
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom'; // imports jest-dom for ALL tests
+import { TextEncoder, TextDecoder } from 'util';
 import dotenv from 'dotenv';
 
 // set up environment variables
@@ -7,3 +7,7 @@ dotenv.config({ path: './.env.local' });
 
 // suppress jest vs. mongoose warnings
 process.env.SUPPRESS_JEST_WARNINGS = 'true';
+
+// hack TextEncoder and TextDecoder back into jest's weird jsdom environment
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder as any;
